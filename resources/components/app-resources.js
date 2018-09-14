a8e0e64c9edf057a084ba256965f8c424c62e6b0
ibrahim <ibrahimbeladi@outlook.com>
2018-09-14 19:59:09 +0300

    dividing arabic and english

--- resources/components/app-resources.js
+++ resources/components/app-resources.js
@@ -1,8 +1,12 @@
 Vue.component("app-resources", {
     template: `
         <div class="container">
+            <h2 style="border-bottom: 1px solid lightgray">
+                المصادر العربية
+            </h2>
+            <br/>
             <div class="row">
-                <div class="col-sm-4" v-for="item in items">
+                <div class="col-sm-4" v-for="item in arabicResources">
                     <div class="card">
                         <div class="card-header">
                             <h5 class="card-title" style="margin: 0;">
@@ -20,6 +24,32 @@ Vue.component("app-resources", {
                     </div>
                 </div>
             </div>
+            <br/>
+            <br/>
+            <br/>
+            <h2 style="border-bottom: 1px solid lightgray">
+                المصادر الانغليزية
+            </h2>
+            <br/>
+            <div class="row">
+                <div class="col-sm-4" v-for="item in englishResources">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title" style="margin: 0;">
+                                {{ item.title }}
+                            </h5>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                {{ item.description }}
+                            </p>
+                            <a :href="item.link" class="btn btn-primary" target="_blank">
+                                فتح المصدر
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>                
         </div>
     `,
     data: function () {
@@ -27,16 +57,28 @@ Vue.component("app-resources", {
             items: [{
                     title: "مدونة vuejsdevelopers",
                     link: "https://vuejsdevelopers.com/",
-                    description: "مدونة باللغة الإنجليزية مخصصة لشروحات ڤيو مليانة شروحات ومواضيع ممكن تفيد المتعلمين",
-                    isArabic: false
+                    description: "مدونة باللغة الإنجليزية مخصصة لشروحات ڤيو مليانة شروحات ومواضيع ممكن تفيد المتعلمين"
                 },
                 {
                     title: "دورة من laracasts",
                     link: "https://laracasts.com/series/learn-vue-2-step-by-step",
-                    description: "دورة Learn Vue 2: Step By Step مقدمة من laracasts لتعلم ڨيو من الصفر",
-                    isArabic: false
+                    description: "دورة Learn Vue 2: Step By Step مقدمة من laracasts لتعلم ڨيو من الصفر"
+                },
+                {
+                    title: "سلسلة تعلم برمجة Vuejs",
+                    link: "https://www.youtube.com/watch?v=ZH50a_ZbxFA&list=PLXgJ7cArk9uSB0kiR1ALOOOpiwJbd93b8",
+                    description: "سلسلة فيديو في اليتيوب لتعلم ڨيو",
+                    isArabic: true
                 }
             ]
         }
+    },
+    computed: {
+        arabicResources: function () {
+            return this.items.filter(item => item.isArabic);
+        },
+        englishResources: function () {
+            return this.items.filter(item => !item.isArabic);
+        },
     }
 });
